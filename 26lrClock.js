79636bf103a912c766b70fa2e22176b4c2eb2778
26LourJohn <136504337+26LourJohn@users.noreply.github.com>
2023-12-15 21:40:29 -0500

    Sundial Time started
    Started adding code for showing local Sun time

--- 26lrClock.js
+++ 26lrClock.js
@@ -66,3 +66,83 @@ Date.prototype.toJulian = function () {
   	timepart -= Math.floor(timepart);
     return datepart+timepart;
 };
+ 
+Date.prototype.equationoftime = function(){
+
+	      /*
+         Equation of Time calculations copied from
+         JavaScript Sun Calculator
+         © 2001 Juergen Giesen
+		 http://www.jgiesen.de
+		 */
+             
+        function RightAscension(T) {
+            var K = Math.PI/180.0
+            var L, M, C, lambda, RA, eps, delta, theta
+            L = sunL(T)
+            M = 357.52910 + 35999.05030*T - 0.0001559*T*T - 0.00000048*T*T*T
+            M = M % 360
+            if (M<0) M = M + 360
+            C = (1.914600 - 0.004817*T - 0.000014*T*T)*Math.sin(K*M)
+            C = C + (0.019993 - 0.000101*T)*Math.sin(K*2*M)
+            C = C + 0.000290*Math.sin(K*3*M)
+            theta = L + C; // true longitude of the Sun
+            eps = EPS(T)
+            eps = eps + 0.00256*Math.cos(K*(125.04 - 1934.136*T))
+            lambda = theta - 0.00569 - 0.00478*Math.sin(K*(125.04 - 1934.136*T)); // apparent longitude of the Sun
+            RA = Math.atan2(Math.cos(K*eps)*Math.sin(K*lambda), Math.cos(K*lambda))
+            RA = RA/K
+            if (RA<0) RA = RA + 360.0;
+            delta = Math.asin(Math.sin(K*eps)*Math.sin(K*lambda))
+            delta = delta/K
+            DEC = delta
+            return RA
+        }
+        
+        function sunL(T){
+            var L = 280.46645 + 36000.76983*T + 0.0003032*T*T
+            L = L % 360
+            if (L<0) L = L + 360
+            return L
+        }
+        
+        function deltaPSI(T){
+            var K = Math.PI/180.0
+            var deltaPsi, omega, LS, LM
+            LS = sunL(T)
+            LM = 218.3165 + 481267.8813*T
+            LM = LM % 360
+            if (LM<0) LM = LM + 360
+            omega = 125.04452 - 1934.136261*T + 0.0020708*T*T + T*T*T/450000
+            deltaPsi = -17.2*Math.sin(K*omega) - 1.32*Math.sin(K*2*LS) - 0.23*Math.sin(K*2*LM) + 0.21*Math.sin(K*2*omega)
+            deltaPsi = deltaPsi/3600.0
+            return deltaPsi
+        }
+        
+        function EPS(T) {
+            var K = Math.PI/180.0
+            var LS = sunL(T)
+            var LM = 218.3165 + 481267.8813*T
+            var eps0 =  23.0 + 26.0/60.0 + 21.448/3600.0 - (46.8150*T + 0.00059*T*T - 0.001813*T*T*T)/3600
+            var omega = 125.04452 - 1934.136261*T + 0.0020708*T*T + T*T*T/450000
+            var deltaEps = (9.20*Math.cos(K*omega) + 0.57*Math.cos(K*2*LS) + 0.10*Math.cos(K*2*LM) - 0.09*Math.cos(K*2*omega))/3600
+            return eps0 + deltaEps
+        }
+        
+	var timepart = this.valueOf()/86400000 + .5;
+	var datepart = Math.floor(timepart) + 2440587;
+	timepart -= Math.floor(timepart);
+	var JD = datepart+timepart;
+	var K = Math.PI/180.0
+	var  T = (JD - 2451545.0) / 36525.0
+	var eps = EPS(T);
+	var RA = RightAscension(T)
+	var LS = sunL(T)
+	var deltaPsi = deltaPSI(T)
+	var E = LS - 0.0057183 - RA + deltaPsi*Math.cos(K*eps)
+	if (E>5) E = E - 360.0
+	E = E*4; // deg. to min
+	E = Math.round(1000*E)/1000
+	return E
+	
+}
