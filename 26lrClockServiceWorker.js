6a9ab7907fe4622258b844415aee14ffa7bdaabb
26LourJohn <136504337+26LourJohn@users.noreply.github.com>
2024-11-02 21:39:47 -0400

    audit changes

--- 26lrClockServiceWorker.js
+++ 26lrClockServiceWorker.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", function(e) {
     e.waitUntil(
         caches.open("26lrClock").then(function(cache) {
-            return cache.addAll(["26lrClock.html",26lrClock.js","26LRLOGO72.png"])
+            return cache.addAll(["26lrClock.html","26lrClock.js","26LRLOGO72.png"])
         })
     )
 })
